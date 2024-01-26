42d354b04d42223de372c54b3bf918ca22cb2ad1
Jessica Wang <wan00967@umn.edu>
2024-01-26 17:25:16 -0600

    Analysis complete

--- R/week2.R
+++ R/week2.R
@@ -1,2 +1,11 @@
 # Import and Labeling
 rt_df <- read.csv("data/week2.csv", header=TRUE)
+rt_df$condition <- factor(rt_df$condition, levels = c("A", "B"), labels = c("Control", "Experimental"))
+rt_df$gender <- factor(rt_df$gender, levels = c("M", "F", "N"), labels = c("Male", "Female", "Nonbinary"))
+
+# Analysis
+mean(rt_df$rt)
+rt_f_df <- subset(rt_df, gender=="Female")
+hist(rt_f_df$rt)              
+datasets <- list(rt_df, rt_f_df)
+datasets[[1]]["rt"]
